0f299e627e639d65182c308d374d48b8ccee0d9e
chaudhuree <chaudhuree00@gmail.com>
2022-11-09 18:01:41 +0600

    docs: export from another file and props & its use

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,11 @@
-import styled from "styled-components";
+import BasicTitle from "./components/BasicTitle"
 function App() {
 
   return (
     <div >
-      <BasicTitle>chaudhuree</BasicTitle>
-      <DefaultButton>click <span>me</span></DefaultButton>
+      <BasicTitle special>chaudhuree</BasicTitle>
     </div>
   )
 }
 
-const BasicTitle = styled.h1`
-  text-align: center;
-  text-transform: uppercase;
-`;
-const DefaultButton = styled.button`
-background: #645cff;
-color:#fff;
-border:none;
-border-radius:0.25rem;
-cursor: pointer;
-text-transform:capitalize ;
-padding: 0.25rem;
-display: block;
-width: 200px;
-margin: 1rem auto;
-span{
-  color:salmon;
-}
-&:hover{
-  background-color:#433aee;
-}
-`
 export default App
---
+++ src/components/BasicTitle.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const BasicTitle = styled.h1`
+text-align: center;
+text-transform: uppercase;
+/* color: ${(props)=> props.special && 'palegreen'}; */
+/* destructured ⬇⬇*/
+/* color: ${({special})=> special && 'palegreen'}; */
+/* ternary operator ⬇⬇ */
+color: ${({special})=> special ? 'palegreen' : 'salmon'};
+`;
+
+export default BasicTitle;
