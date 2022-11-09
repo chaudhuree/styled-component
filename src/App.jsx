dba1c819c205767c7f92ce3d53d4858d8cfb46b6
chaudhuree <chaudhuree00@gmail.com>
2022-11-09 17:46:27 +0600

    basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
+import styled from "styled-components";
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div >
+      <BasicTitle>chaudhuree</BasicTitle>
+      <DefaultButton>click <span>me</span></DefaultButton>
     </div>
   )
 }
 
+const BasicTitle = styled.h1`
+  text-align: center;
+  text-transform: uppercase;
+`;
+const DefaultButton = styled.button`
+background: #645cff;
+color:#fff;
+border:none;
+border-radius:0.25rem;
+cursor: pointer;
+text-transform:capitalize ;
+padding: 0.25rem;
+display: block;
+width: 200px;
+margin: 1rem auto;
+`
 export default App
